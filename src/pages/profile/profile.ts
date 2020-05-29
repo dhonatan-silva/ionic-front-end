3c089dbb0728d7e1c5e94d1c585432c848b79612
Dhonatan da Silva <dhonatan_silva@hotmail.com>
2020-05-28 22:07:36 -0300

    Bugfix - atualizacao da imagem de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,6 +5,7 @@ import { StorageService } from '../../services/storage.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @IonicPage()
 @Component({
@@ -15,6 +16,7 @@ export class ProfilePage {
 
   cliente: ClienteDTO;
   picture: string;
+  profileImage;
   cameraOn: boolean = false;
 
   constructor(
@@ -22,7 +24,10 @@ export class ProfilePage {
     public navParams: NavParams,
     public storage: StorageService,
     public clienteService: ClienteService,
-    public camera: Camera) {
+    public camera: Camera,
+    public sanitizer: DomSanitizer) {
+      
+      this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -37,7 +42,7 @@ export class ProfilePage {
         this.cliente = response as ClienteDTO;
         this.getImageIfexist();
       },
-      error =>{
+      error => {
         if (error.status == 403) {
          this.navCtrl.setRoot('Homepage');
         }
@@ -50,9 +55,24 @@ export class ProfilePage {
   getImageIfexist(){
     this.clienteService.getImageFromBucket(this.cliente.id)
     .subscribe(response => {
-      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`
+      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      this.blobToDataURL(response).then(dataUrl => {
+        let str: string = dataUrl as string;
+        this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+      });
     }, 
-    error => {});
+    error => {
+      this.profileImage = 'assets/imgs/avatar-blank.png';
+    });
+  }
+
+  blobToDataURL(blob) {
+    return new Promise((fullfill, reject) => {
+      let reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = (e) => fullfill(reader.result);
+      reader.readAsDataURL(blob);
+    });
   }
 
   getCameraPicture() {
@@ -97,9 +117,9 @@ export class ProfilePage {
     this.clienteService.uploadPicture(this.picture)
     .subscribe(response => {
       this.picture = null;
-      this.loadData();
+      this.getImageIfexist();
     },
-    erro => {
+    error => {
 
     });
   }
